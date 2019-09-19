1c0fcfd83f1ddb728b6ff71dae6605fe83624225
Kate Morris <kmmorris@deloitte.co.uk>
2019-09-19 10:49:11 +0100

    Introduced button and increment counter

--- src/App.test.js
+++ src/App.test.js
@@ -7,6 +7,17 @@ import App from './App';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
+/**
+ * Factory function to create a ShallowWrapper for the App component
+ * @function setup
+ * @param {object} props - Component porps specific to this setup
+ * @returns {ShallowWrapper} 
+*/ 
+const setup = (props={}, state =null) => {
+  return shallow(<App {...props} />)
+}
+
+/**  */
 test('renders without error', () => {
   const wrapper = shallow(<App />);
   const appComponent = wrapper.find("[data-test='component-app']")
@@ -14,11 +25,15 @@ test('renders without error', () => {
 })
 
 test('renders increment button', () => {
-
+  const wrapper = shallow(<App />);
+  const button = wrapper.find("[data-test='increment-button']")
+  expect(button.length).toBe(1); // Find 1 element
 })
 
 test('renders counter display', () => {
-
+  const wrapper = shallow(<App />);
+  const counterDisplay = wrapper.find("[data-test='counter-display']")
+  expect(counterDisplay.length).toBe(1); // Find 1 element
 })
 
 test('counter starts at 0', () => {
